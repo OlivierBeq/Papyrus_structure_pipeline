ddc229439929904c0441e2a6c901a119baf66fcd
OlivierBeq <35777118+OlivierBeq@users.noreply.github.com>
2023-07-25 08:51:16 +0200

    Add molecular charge neutralization

--- src/papyrus_structure_pipeline/__init__.py
+++ src/papyrus_structure_pipeline/__init__.py
@@ -5,4 +5,4 @@
 
 from .standardizer import standardize  # noqa: F401
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- src/papyrus_structure_pipeline/standardizer.py
+++ src/papyrus_structure_pipeline/standardizer.py
@@ -16,7 +16,7 @@ from typing import Optional, Tuple, Union
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from rdkit.Chem.MolStandardize.rdMolStandardize import TautomerEnumerator
+from rdkit.Chem.MolStandardize.rdMolStandardize import TautomerEnumerator, Uncharger
 from rdkit.rdBase import BlockLogs
 
 # Remove warnings of importing the ChEMBL Structure Pipeline
@@ -53,7 +53,6 @@ SALTS = ['[Na+]', '[K+]', 'Cl', 'C1=NC=CC=C1', '[O-][Cl+3]([O-])([O-])[O-]', 'O=
          'C', 'C1COCCO1', 'CCCCCCN(C)C', 'CCCCCC', 'CN(C)C(=N)N', 'CN(C)C',
          'c1ccc([B-](c2ccccc2)(c2ccccc2)c2ccccc2)cc1']
 
-
 # Define metals as all but H, B, C, N, O, P, F, S, Cl, Br, I
 METALS = ["He", "Li", "Be", "Ne", "Na", "Mg", "Al", "Si", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co",
           "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd",
@@ -62,7 +61,6 @@ METALS = ["He", "Li", "Be", "Ne", "Na", "Mg", "Al", "Si", "Ar", "K", "Ca", "Sc",
           "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md",
           "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"]
 METALS = [f'[{metal}]' for metal in METALS]
-
 
 # Define organic atoms
 ORGANIC_ATOMS = ['C', 'H', 'O', 'N', 'P', 'S', 'F', 'Cl', 'I', 'Br']
@@ -73,6 +71,7 @@ def standardize(mol: Chem.Mol,
                 remove_additional_metals: bool = True,
                 filter_mixtures: bool = True,
                 filter_inorganic: bool = True,
+                uncharge: bool = True,
                 filter_non_small_molecule: bool = True,
                 canonicalize_tautomer: bool = True,
                 small_molecule_min_mw: float = 200,
@@ -86,13 +85,13 @@ def standardize(mol: Chem.Mol,
 
     Steps:
     1) Use the ChEMBL structure pipeline (get parent molecule & standardization)
-    2) If extended:
-        2.1) remove supplementary salts & metals
-        2.2) check if a mixture
-        2.3) check if organic
-        2.4) check if 200 <= MW <= 800
-    3) canonicalize tautomer
-    4) Use the ChEMBL structure pipeline (get parent molecule & standardization)
+    2) remove supplementary salts & metals
+    3) check if a mixture
+    4) check if organic
+    5) uncharge
+    5) check if 200 <= MW <= 800
+    6) canonicalize tautomer
+    7) Use the ChEMBL structure pipeline (get parent molecule & standardization)
 
     :param mol: RDKit molecule to be standardized
     :param remove_additional_salts: should additional salts then those
@@ -100,13 +99,16 @@ def standardize(mol: Chem.Mol,
     :param remove_additional_metals: should salt removal also remove metal atoms
     :param filter_mixtures: should mixtures be filtered out
     :param filter_inorganic: should inorganic molecules be filtered out
-    :param filter_non_small_molecule: should non-small molecules be filtred out
+    :param uncharge: should formal charges be removed (based on a list of substructures)
+    :param filter_non_small_molecule: should non-small molecules be filtered out
     :param canonicalize_tautomer: should a canonical tautomer be determined
     :param small_molecule_min_mw: minimum molecular weight to be a valid small molecule
     :param small_molecule_max_mw: maximum molecular weight to be a valid small molecule
-    :param tautomer_allow_stereo_removal: allow stereocenters to be remove by tautomerization
+    :param tautomer_allow_stereo_removal: allow stereocenters to be removed by tautomerization
     :param tautomer_max_tautomers: maximum number of tautomers to enumerate (ignored if 0)
     :param return_type: If True, include the `StandardizationResult` in the return value
+    :param raise_error: If True, standardization failures raise exceptions, if False, None is returned
+    (with the standardization error if `return_type` is set).
     :return: a tuple of either the standardized molecule or None and a standardization_result flag
     """
     # Apply ChEMBL standardization
@@ -121,6 +123,9 @@ def standardize(mol: Chem.Mol,
         # Ensure organic
         if filter_inorganic and not is_organic(std_mol):
             return (None, StandardizationResult.INORGANIC_MOLECULE) if return_type else None
+        # Remove charges
+        if uncharge:
+            std_mol = _uncharge(std_mol)
         # Ensure small molecule
         if filter_non_small_molecule and not is_small_molecule(std_mol,
                                                                min_molwt=small_molecule_min_mw,
@@ -178,13 +183,14 @@ def _canonicalize_tautomer(mol: Chem.Mol, allow_stereo_removal: bool = True,
     enumerator.SetMaxTautomers(max_tautomers)
     # Enumerate tautomers
     if allow_stereo_removal:
+        enumerator.SetRemoveSp3Stereo(True)
+        enumerator.SetRemoveBondStereo(True)
         tautos = enumerator.Enumerate(mol)
     else:
         enumerator.SetRemoveSp3Stereo(False)
+        enumerator.SetRemoveBondStereo(False)
         tautos = enumerator.Enumerate(mol)
-
-        # Keep those with stereo unchanged if required
-
+        # Keep tautomers with stereo unchanged if required
         def get_num_ciral_centers(mol_: Chem.Mol):
             """Utility function."""
             return len([atom.GetChiralTag()
@@ -303,3 +309,44 @@ def _remove_supplementary_salts(mol: Chem.Mol,
         for frag in frags[1:]:
             res = Chem.CombineMols(res, frag)
         return res if not return_type else (res, SaltStrippingResult.STRIPPED_MOLECULE)
+
+
+def _uncharge(mol: Chem.Mol) -> Chem.Mol:
+    """Uncharge the given RDKit molecule.
+
+    Adapted from Hans de Winter's contribution to B3DB
+    (https://github.com/theochem/B3DB/blob/main/cleaning/02_clean_smiles_chembl_way_20210215.py)
+    """
+    if mol is None:
+        raise ValueError('A RDKit molecule must be specified')
+    uncharger = Uncharger()
+    mol = uncharger.uncharge(mol)
+    # Do a second pass on zwitterions
+    romol = Chem.RWMol(mol)
+    neutralizing_smiles = [  # Imidazoles
+        ('[n+;H]', 'n'),
+        # Amines
+        ('[N+;!H0]', 'N'),
+        # Carboxylic acids and alcohols
+        ('[$([O-]);!$([O-][#7])]', 'O'),
+        # Thiols
+        ('[S-;X1]', 'S'),
+        # Sulfonamides
+        ('[$([N-;X2]S(=O)=O)]', 'N'),
+        # Enamines
+        ('[$([N-;X2][C,N]=C)]', 'N'),
+        # Tetrazoles
+        ('[n-]', '[nH]'),
+        # Sulfoxides
+        ('[$([S-]=O)]', 'S'),
+        # Amides
+        ('[$([N-]C=O)]', 'N')
+    ]
+    neutralizing_reactions = [(Chem.MolFromSmarts(x), Chem.MolFromSmiles(y, False)) for x, y in neutralizing_smiles]
+    for i, (reactant, product) in enumerate(neutralizing_reactions):
+        while romol.HasSubstructMatch(reactant):
+            rms = AllChem.ReplaceSubstructs(romol, reactant, product)
+            romol = rms[0]
+    mol = Chem.Mol(romol)
+    Chem.SanitizeMol(mol)
+    return mol
